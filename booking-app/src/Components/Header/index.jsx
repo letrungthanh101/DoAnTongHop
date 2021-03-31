be8fd4214daa6f0e6e454906fdc8e6dca01a33c3
Trung Thanh <letrungthanh101@gmail.com>
2021-03-31 10:12:17 +0700

    Thành commit sửa html , css phần header

--- booking-app/src/Components/Header/index.jsx
+++ booking-app/src/Components/Header/index.jsx
@@ -4,12 +4,12 @@ import './header.scss';
 import logo from '../../Assets/Images/logo.svg';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-
-import { IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton, Menu } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
+
 import { Link, NavLink } from 'react-router-dom';
 Header.propTypes = {};
-
 const useStyles = makeStyles({
   root: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -20,49 +20,70 @@ const useStyles = makeStyles({
     height: 33,
     padding: '0 30px',
   },
+  btn: {
+    color: 'white',
+  },
+ 
 });
-
 function Header(props) {
   const classes = useStyles();
   return (
     <div>
-      <header className="container-fluid">
-        
-        <nav className="nav container navbar-expand-sm " id="navbarNavDropdown">
-          <div className="top-nav top-nav--left">
-            <ul className="top-nav__ul  top-nav__ul--left">
-              <li>
-                <NavLink to="/home" className="li__item  li__item--hover" activeClassName="active-menu"> HOME</NavLink>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light ">
+          <div className="top-nav--middle logo-brand">
+          <Link to="/home" exact>
+                <img src={logo} alt="logo" />
+              </Link>
+          </div>
+           
+          <IconButton    className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <MenuIcon className={classes.btn}/>
+          </IconButton>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto navbar-flex">
+              <li className="nav-item active">
+                <NavLink to="/home" className="li__item  li__item--hover  " activeClassName="active-menu">
+                  HOME
+                </NavLink>
               </li>
-              <li>
-                <NavLink to="/store" className="li__item  li__item--hover"   activeClassName="active-menu">STORE</NavLink>
+              <li className="nav-item">
+                <NavLink to="/store" className="li__item  li__item--hover " activeClassName="active-menu">
+                  STORE
+                </NavLink>
               </li>
-              <li>
-                <NavLink to="/booking" className="li__item  li__item--hover"  activeClassName="active-menu">BOOKING</NavLink>
+              <li className="nav-item">
+                <NavLink to="/booking" className="li__item  li__item--hover " activeClassName="active-menu">
+                  BOOKING
+                </NavLink>
               </li>
             </ul>
-          </div>
-          <div className="top-nav--middle logo">
-            <Link to ="/home" exact>
-              <img src={logo} alt="logo" />
-            </Link>
-          </div>
-          <div className=" top-nav top-nav--right">
-            <ul className="top-nav__ul  top-nav__ul--right">
-              <li>
-                <NavLink to="/blog" className="li__item  li__item--hover"  activeClassName="active-menu">BLOG</NavLink>
+            <div className="top-nav--middle logo  mr-auto navbar-flex">
+              <Link to="/home" exact>
+                <img src={logo} alt="logo" />
+              </Link>
+            </div>
+            <ul className="navbar-nav  navbar-flex">
+              <li className="nav-item active">
+                <NavLink to="/blog" className="li__item  li__item--hover " activeClassName="active-menu">
+                  BLOG
+                </NavLink>
               </li>
-              <li>
-                <NavLink to="/owner" className="li__item  li__item--hover"  activeClassName="active-menu">FOR OWNER</NavLink>
+              <li className="nav-item">
+                <NavLink to="/owner" className="li__item  li__item--hover " activeClassName="active-menu">
+                  FOR OWNER
+                </NavLink>
               </li>
-              <li>
-                <Button className={classes.root}
-                startIcon = {<PersonIcon />}
-                >
-                  
+              <li className="nav-item ">
+                <Button className={classes.root} startIcon={<PersonIcon />}>
                   <Link to="/login">LOGIN</Link>
                 </Button>
-                ;
               </li>
             </ul>
           </div>
